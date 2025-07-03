5e407d510c522fa4e5b276053a3b5679f6fa8c9b
Rashidul Rahul <rashidul.rahul@gmail.com>
2025-07-03 22:29:21 +0600

    update allowed host

--- rahul/settings.py
+++ rahul/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e+31eulg20mwgmydge&m4t51+dfxp%ae!%!cd#(7w5ghm5*7#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
